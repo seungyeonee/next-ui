f0edbfca9e4cd537388355306864ec53b204c851
Seung yeon Lee <105406805+seungyeonee@users.noreply.github.com>
2024-06-05 18:01:14 +0900

    240605_3

--- src/app/(components)/button/page.tsx
+++ src/app/(components)/button/page.tsx
@@ -1,42 +1,45 @@
 'use client'
 import { Button } from "@/components";
 import { handleCopyClipBoard } from "@/utils/common";
+import { Suspense } from "react";
 
 export default function ButtonPage() {
     return (
-        <section className="card">
-            <h2 className="h2">Button</h2>
-            <p className="txt">Display a button</p>
-            <h3 className="h3 sub">Props</h3>
-            <ul className="props">
-                <li className="name">size</li>
-                <li className="type">string</li>
-                <li className="init">sm / <b>md</b> / lg</li>
-                <li className="expl">button size</li>
-            </ul>
-            <ul className="props">
-                <li className="name">disabled</li>
-                <li className="type">boolean</li>
-                <li className="init">-</li>
-                <li className="expl">button disabled check</li>
-            </ul>
-            <ul className="props">
-                <li className="name">spacing</li>
-                <li className="type">number</li>
-                <li className="init">-</li>
-                <li className="expl">button margin top (px)</li>
-            </ul>
-            <h3 className="h3 sub">Preview</h3>
-            <Button>
-                BUTTON TEXT
-            </Button>
-            <h3 className="h3 sub">Code</h3>
-            <code>
-                <button className="btn-copy" onClick={() => {
-                    handleCopyClipBoard('<Button>BUTTON TEXT</Button>')
-                }}>copy</button>
-                {`<Button>BUTTON TEXT</Button>`}
-            </code>
-        </section>
+        <Suspense fallback={<div>Loading...</div>}>
+            <section className="card">
+                <h2 className="h2">Button</h2>
+                <p className="txt">Display a button</p>
+                <h3 className="h3 sub">Props</h3>
+                <ul className="props">
+                    <li className="name">size</li>
+                    <li className="type">string</li>
+                    <li className="init">sm / <b>md</b> / lg</li>
+                    <li className="expl">button size</li>
+                </ul>
+                <ul className="props">
+                    <li className="name">disabled</li>
+                    <li className="type">boolean</li>
+                    <li className="init">-</li>
+                    <li className="expl">button disabled check</li>
+                </ul>
+                <ul className="props">
+                    <li className="name">spacing</li>
+                    <li className="type">number</li>
+                    <li className="init">-</li>
+                    <li className="expl">button margin top (px)</li>
+                </ul>
+                <h3 className="h3 sub">Preview</h3>
+                <Button>
+                    BUTTON TEXT
+                </Button>
+                <h3 className="h3 sub">Code</h3>
+                <code>
+                    <button className="btn-copy" onClick={() => {
+                        handleCopyClipBoard('<Button>BUTTON TEXT</Button>')
+                    }}>copy</button>
+                    {`<Button>BUTTON TEXT</Button>`}
+                </code>
+            </section>
+        </Suspense>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,26 @@
 import { List, Button } from "@/components";
+import { Suspense } from "react";
 
 export default function MainPage() {
   return (
-    <section className="card">
-      <h1 className="h1">NEXT-UI</h1>
-      <p className="txt mb-4">Next-UI is a powerful and flexible UI library built with Next.js and TypeScript. Designed to boost developer productivity and code quality, it offers:</p>
-      <hr/>
-      <List className="mt-4 mb-4">
-        <li>Type Safety: TypeScript ensures type correctness at compile-time, preventing runtime errors.</li>
-        <li>Optimized Bundle Size: Code-splitting and tree-shaking techniques minimize bundle size for fast load times.</li>
-        <li>Extensible Design System: Easily implement your brand{`'`}s design with default design tokens and theming.</li>
-        <li>Accessibility Compliance: Follows WAI-ARIA standards for web accessibility.</li>
-        <li>Comprehensive Component Library: Provides a rich set of UI components like buttons, inputs, modals, tooltips, and tables.</li>
-      </List>
-      <hr/>
-      <p className="txt mt-4 mb-4">Next-UI combines high development velocity with robust code quality. Its simple usage boosts productivity while its powerful features and extensibility handle complex requirements. Leverage Next-UI for your Next.js and React projects today.</p>
-      <Button disabled>
-        See You Soon...
-      </Button>
-    </section>
+    <Suspense fallback={<div>Loading...</div>}>
+      <section className="card">
+        <h1 className="h1">NEXT-UI</h1>
+        <p className="txt mb-4">Next-UI is a powerful and flexible UI library built with Next.js and TypeScript. Designed to boost developer productivity and code quality, it offers:</p>
+        <hr />
+        <List className="mt-4 mb-4">
+          <li>Type Safety: TypeScript ensures type correctness at compile-time, preventing runtime errors.</li>
+          <li>Optimized Bundle Size: Code-splitting and tree-shaking techniques minimize bundle size for fast load times.</li>
+          <li>Extensible Design System: Easily implement your brand{`'`}s design with default design tokens and theming.</li>
+          <li>Accessibility Compliance: Follows WAI-ARIA standards for web accessibility.</li>
+          <li>Comprehensive Component Library: Provides a rich set of UI components like buttons, inputs, modals, tooltips, and tables.</li>
+        </List>
+        <hr />
+        <p className="txt mt-4 mb-4">Next-UI combines high development velocity with robust code quality. Its simple usage boosts productivity while its powerful features and extensibility handle complex requirements. Leverage Next-UI for your Next.js and React projects today.</p>
+        <Button disabled>
+          See You Soon...
+        </Button>
+      </section>
+    </Suspense>
   );
 }
